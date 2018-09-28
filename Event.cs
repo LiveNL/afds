d10736a72cae8df55f077b68e198b0f29e3d26b5
LiveNL <jordiwippert@live.nl>
2018-09-28 12:43:45 +0200

    Create EventList

---
+++ Event.cs
@@ -0,0 +1,7 @@
+using System;
+
+namespace afds {
+  public class Event {
+    public int DateTime { get; set; }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace afds {
   class Program {
     static void Main(string[] args) {
       Uithoflijn uithoflijn = new Uithoflijn();
       State state           = new State();
+      List<Event> events    = new List<Event>();
 
       bool endCondition = false;
       while (endCondition == false) {
-        timingRoutine(uithoflijn, state);
-        eventRoutine(uithoflijn, state);
+        timingRoutine(uithoflijn, state, events);
+        eventRoutine(uithoflijn, state, events);
 
         if (state.SimulationClock > DateTime.Parse("7:00:00 PM")) {
           endCondition = true;
@@ -20,12 +22,11 @@ namespace afds {
       (new Report()).Print(uithoflijn, state);
     }
 
-    // TODO: Create dynamic eventlist
-    static void timingRoutine(Uithoflijn uithoflijn, State state) {
+    static void timingRoutine(Uithoflijn uithoflijn, State state, List<Event> events) {
       Console.WriteLine(state.SimulationClock);
     }
 
-    static void eventRoutine(Uithoflijn uithoflijn, State state) {
+    static void eventRoutine(Uithoflijn uithoflijn, State state, List<Event> events) {
       uithoflijn.Update(uithoflijn, state);
       Console.WriteLine(state.SimulationClock);
     }
