a9a34a74592fb42fae25f12e2be03b82e36d8ea6
Thomas Kok <thomaskok93@gmail.com>
2018-10-16 15:34:02 +0200

    Probability functions added (not final)

---
+++ Probabilities.cs
@@ -0,0 +1,183 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using MathNet.Numerics.Random;
+
+namespace Probabilities
+{
+    class Program
+    {
+        //Declaring global variables.
+        private static MersenneTwister random;
+        static int[] runtimes_a;
+        static int[] runtimes_b;
+        static Dictionary<string, double[]> rates_a;
+        static Dictionary<string, double[]> rates_b;
+
+        public static int[] Runtimes_a { get => runtimes_a; set => runtimes_a = value; }
+        public static int[] Runtimes_b { get => runtimes_b; set => runtimes_b = value; }
+        public static Dictionary<string, double[]> Rates_a { get => rates_a; set => rates_a = value; }
+        public static Dictionary<string, double[]> Rates_b { get => rates_b; set => rates_b = value; }
+
+        static void Main(string[] args)
+        {
+            //Initializing global variables.
+            random = new MersenneTwister(12);
+            Runtimes_a = new int[] { 110, 78, 82, 60, 100, 59, 243, 135 };
+            Runtimes_b = new int[] { 134, 243, 59, 101, 60, 86, 78, 113 };
+            const string Filepath = "C:/Users/thoma/Documents/ADS/Probabilities/Data/rates_a.csv";
+            Rates_a = ReadCsv(Filepath);
+            const string Filepath1 = "C:/Users/thoma/Documents/ADS/Probabilities/Data/rates_b.csv";
+            Rates_b = ReadCsv(Filepath1);
+
+            while (true)
+            {
+                //Statements for testing.
+                DateTime begin = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 7, 0, 0);
+                DateTime end = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 19, 0, 0);
+                Console.WriteLine(GeneratePassengerArrivals(begin, end, rates_b["CS Centrumzijde"]));
+                System.Threading.Thread.Sleep(100);
+            }
+            Console.ReadKey();
+        }
+
+        //The functions that are directly called in the simulation.
+        static int CalcRunTime(int mean)
+        {
+            return (int)CalcLogNormal(Math.Log((double)mean), 0.321 * ((double)mean - 49.38125) / (49.38125 + 143.13026));
+        }
+
+        static int CalcDwellingTime(int passengers_in, int passengers_out)
+        {
+            double mean = 12.5 + 0.22 * passengers_in + 0.13 * passengers_out;
+            double min = 0.8 * mean;
+            double gamma = GenerateGammaValue(2, mean);
+            if (gamma < min) return (int)min;
+            else return (int)gamma;
+        }
+
+        static int CalcSecondDwellingTime(int passengers_in)
+        {
+            double mean = 0.22 * passengers_in;
+            double min = 0.8 * mean;
+            double gamma = GenerateGammaValue(2, mean);
+            if (gamma < min) return (int)min;
+            else return (int)gamma;
+        }
+
+        static int GeneratePassengerArrivals(DateTime begin, DateTime end, double[] rates)
+        {
+            int res = 0;
+            int begin_i = TimeToIndex(begin);
+            int end_i = TimeToIndex(end);
+            double seconds = begin.Second + (60.0 * begin.Minute);
+            if (rates[begin_i] == 0) return 0;
+            int j = begin_i;
+            while (seconds <= (double)end.Second + 60.0 * end.Minute + 3600.0 * (end.Hour - begin.Hour))
+            {
+                double exp = CalcExp(rates[j]);
+
+                if (seconds + exp > (double)end.Second + 60.0 * end.Minute + 3600.0 * (end.Hour - begin.Hour))
+                {
+                    double end_time = (double)end.Second + 60.0 * end.Minute + 3600.0 * (end.Hour - begin.Hour);
+                    double fraction = (end_time - seconds) / exp;
+                    if (fraction >= random.NextDouble())
+                        res++;
+                    break;
+                }
+
+                if ((int)((seconds + exp) / 900) > (int)(seconds / 900))
+                {
+                    j++;
+                    double goto_time = (double)((int)(seconds + exp) / 900) * 900.0;
+                    double fraction = (goto_time - seconds) / exp;
+                    if (fraction < random.NextDouble())
+                        res--;
+                    seconds = goto_time;
+                }
+
+                res++;
+                seconds += exp;
+            }
+
+            return res;
+        }
+
+        //The probability functions that are used in determining the random variables.
+        static double GenerateGammaValue(int k, double theta)
+        {
+            //calculate for theta equals 1, then multiply by theta
+            double d = k - 1 / 3;
+            double c = 1 / Math.Sqrt(9 * d);
+            while(true)
+            {
+                double x = CalcNormal();
+                double u = random.NextDouble();
+                double v = Math.Pow((1 + c * x), 3);
+                if (v > 0 && Math.Log(u) < 0.5 * (x * x) + d - d * v + d * Math.Log(v))
+                    return d * v * theta;
+            }
+        }
+
+        static double CalcLogNormal(double mu, double sigma)
+        {
+            double y = CalcNormal(mu, sigma);
+            return (Math.Pow(Math.E, y));
+        }
+
+        static double CalcNormal(double mean = 0, double std = 1)
+        {
+            //Calculated using the Box Muller transform
+            double u1 = random.NextDouble();
+            double u2 = random.NextDouble();
+            return (Math.Sqrt(-2 * Math.Log(u1)) * Math.Sin(2 * Math.PI * u2)) * std + mean;
+        }
+
+        static double CalcExp(double rate)
+        {
+            double u = random.NextDouble();
+            return -(1.0 / rate) * Math.Log(u);
+        }
+
+        //Other support functions used.
+        static int TimeToIndex(DateTime time)
+        {
+            //07:00-07:14 -> 0
+            //07:15-07:29 -> 1
+            //...
+            //18:45-18:59 -> 47
+            //exception: 19:00 -> 47
+            DateTime quarter = time.AddMinutes(-(time.Minute % 15));
+            TimeSpan diff = quarter - new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 7, 0, 0);
+            int res = (int)diff.TotalMinutes / 15;
+            return res >= 47 ? 47 : res;
+        }
+
+        static Dictionary<string, double[]> ReadCsv(string filepath)
+        {
+            Dictionary<string, double[]> res = new Dictionary<string, double[]>();
+            using (var reader = new StreamReader(filepath))
+            {
+                List<string> list = new List<string>();
+                var first_line = reader.ReadLine();
+                var names = first_line.Split(';');
+                for (int i = 1; i < names.Length; i++)
+                {
+                    res.Add(names[i], new double[48]);
+                }
+                int n = 0;
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        res[names[i]][n] = double.Parse(values[i]);
+                    }
+                    n++;
+                }
+            }
+            return res;
+        }
+    }
+}
